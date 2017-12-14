6ef598ace8432f30ec3038d2163bb09fba350fcf
Faris van Hien <f.a.vanhien@students.uu.nl>
2017-12-14 12:55:03 +0100

    first commit

---
+++ NetChange/Node.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetChange
+{
+    
+    class Node
+    {
+        string poortnummer;
+        int NodeNum;
+        HashSet<Node> Neigh;   //list of neighbours
+        string[] poortnums;//list of portnumbers
+        string[] Nb;      //preferred neigbour
+        int[] D;        //estimated distance
+        int[,] nDis;     //estimates d(w,v)
+        Node[] V;
+        int N; //number of nodes
+        public Node(string[] _poortnums, HashSet<Node> _Neigh)
+        {
+            initialization();
+            N = V.Length;
+            poortnums = _poortnums;
+            Neigh = _Neigh;
+            D = new int[N];
+            Nb = new string[N];
+            nDis = new int[N, N];
+
+        }
+        public int getIndex(string poort)
+        {
+            for (int i = 0; i < poortnums.Length; i++)
+            {
+                if (poortnums[i] == poort)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        //estimate the distance between two nodes
+        public void d(int u, int v)
+        {
+          
+        }
+        public void initialization()
+        {
+            foreach (Node v in V)
+            {
+                int vi = getIndex(v.poortnummer);
+                foreach (Node w in Neigh)
+                {
+                    int wi = getIndex(w.poortnummer);
+                    nDis[wi, vi] = N;
+                }
+                D[vi] = N;
+                Nb[vi] = "udef";
+            }
+            int ui = getIndex(poortnummer);
+            D[ui] = 0;
+            Nb[ui] = "local";
+            foreach (Node w in Neigh)
+            {
+                w.receiveMessage("mydist", poortnummer, poortnummer, 0);
+            }
+
+
+        }
+        public void recompute(Node v)
+        {
+           
+        }
+        public void receiveMessage(string s, string poortVan, string poortNaar, int dist)
+        {
+            if (isNeigh(poortVan)) //maybe delete
+            {
+                int wi = getIndex(poortVan);
+                int vi = getIndex(poortNaar);
+                nDis[wi, vi] = dist;
+            }
+        }
+        public bool isNeigh(string poort)
+        {
+            foreach (Node w in Neigh)
+            {
+                if (w.poortnummer == poort) return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ NetChange/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetChange netchange = new NetChange();
+        }
+    }
+    class NetChange
+    {
+        string commando;
+        string bericht;
+        int N; 
+
+
+        public NetChange()
+        {
+            Console.WriteLine("How many nodes in the network");
+            N = int.Parse(Console.ReadLine());
+
+
+            string[] input = Console.ReadLine().Split();
+            commando = input[0];    //commando dat uitgevoerd moet worden
+            if (commando != "R") poortnummer = int.Parse(input[1]);
+            if (commando == "B") bericht = input[2];
+
+            switch (commando)
+            {
+                case "R":
+                    routingTable();
+                    break;
+                case "B":
+                    stuurBericht();
+                    break;
+                case "C":
+                    maakVerbinding();
+                    break;
+                case "D":
+                    verbreekVerbinding();
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void routingTable()
+        {
+            foreach (string verbinding in verbindingen)
+            {
+                Console.WriteLine(verbinding);
+            }
+        }
+        public void stuurBericht()
+        {
+
+        }
+        public void maakVerbinding()
+        {
+
+        }
+        public void verbreekVerbinding()
+        {
+
+        }
+    }
+}
