bfdefd7c76fc3e51288d3618102df8f17d65ddd2
Faris van Hien <f.a.vanhien@students.uu.nl>
2017-12-22 16:36:02 +0100

    testjes

---
+++ NetChange/bin/Debug/Test2.bat
@@ -0,0 +1,7 @@
+@echo off
+start NetChange.exe 1100 1102 1101
+start NetChange.exe 1101 1100 1102
+start NetChange.exe 1102 1100 1101 1106 1104
+start NetChange.exe 1104 1102 1105
+start NetChange.exe 1105 1104 1106
+start NetChange.exe 1106 1105 1102
